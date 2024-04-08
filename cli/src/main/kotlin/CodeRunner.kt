347635e346f6d5e6fa054080ec6738f4727d3c8c
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2024-04-07 23:16:46 -0300

    feat: added custom formatter and linter rules usage to cli

--- cli/src/main/kotlin/CodeRunner.kt
+++ cli/src/main/kotlin/CodeRunner.kt
@@ -12,17 +12,12 @@ import org.example.lexer.LexerImpl
 import org.example.parser.ParserImpl
 
 class CodeRunner : CliktCommand() {
-    private val runOption by option("-r", "--run", help = "Run interpreter")
-        .flag()
-
-    private val linterOption by option("-l", "--linter", help = "Run linter")
-        .flag()
-
-    private val formatOption by option("-f", "--format", help = "Run formatter")
-        .flag()
-
+    private val runOption by option("-r", "--run", help = "Run interpreter").flag()
+    private val linterOption by option("-l", "--linter", help = "Run linter").flag()
+    private val formatOption by option("-f", "--format", help = "Run formatter").flag()
+    private val rulesFileOption by option("-rules", "--rules", help = "Custom rules file for formatter or linter")
+        .file(canBeDir = false, canBeFile = true)
     private val inputFile by argument(help = "Input file").file(mustExist = true, canBeDir = false, canBeFile = true)
-
     private val outputFile by argument(help = "Output file for formatter")
         .file(canBeDir = false, canBeFile = true).optional()
 
@@ -32,11 +27,9 @@ class CodeRunner : CliktCommand() {
     override fun run() {
         try {
             inputFile.let { file ->
-                // Read the input file
                 val input = file.readText()
                 val lexer = LexerImpl()
                 val tokens = lexer.tokenize(input)
-
                 val parser = ParserImpl(tokens)
                 val ast = parser.parse()
 
@@ -64,21 +57,28 @@ class CodeRunner : CliktCommand() {
     private fun runLinter(input: ProgramNode) {
         echo("Running linter on file ${inputFile.name}... \n")
         val linter = LinterImpl()
-        linter.checkErrors(input, defaultLinterRules).forEach { echo(it.toString()) }
+        val rulesFile = rulesFileOption?.path ?: defaultLinterRules
+        linter.checkErrors(input, rulesFile).forEach { echo(it.toString()) }
     }
 
     private fun runFormatter(input: ProgramNode) {
+        val rulesFile = rulesFileOption?.path ?: defaultFormatterRules
+        val formattedText = formatFile(input, rulesFile)
         if (outputFile == null) {
             echo("Formatting file ${inputFile.name}... \n")
-            val formatter = FormatterImpl()
-            val formatted = formatter.format(input, defaultFormatterRules)
-            inputFile.writeText(formatted)
+            inputFile.writeText(formattedText)
         } else {
             echo("Formatting file ${inputFile.name} to ${outputFile!!.name}... \n")
-            val formatter = FormatterImpl()
-            val formatted = formatter.format(input, defaultFormatterRules)
-            outputFile!!.writeText(formatted)
+            outputFile!!.writeText(formattedText)
         }
+    }
+
+    private fun formatFile(
+        input: ProgramNode,
+        rulesFile: String,
+    ): String {
+        val formatter = FormatterImpl()
+        return formatter.format(input, rulesFile)
     }
 
     private fun printHelp() {
@@ -87,10 +87,11 @@ class CodeRunner : CliktCommand() {
             |Usage: printscript [OPTIONS] INPUT_FILE OPTIONAL_OUTPUT_FILE
             |
             |Options:
-            |  -r, --run     Run interpreter
-            |  -l, --linter  Run linter
-            |  -f, --format  Run formatter (optional output file)
-            |  -h, --help    Show this message and exit
+            |  -r, --run            Run interpreter
+            |  -l, --linter         Run linter
+            |  -f, --format         Run formatter (can add optional output file)
+            |  --rules=RULES_FILE  Use custom rules for formatter or linter (optional json file)
+            |  -h, --help           Show this message and exit
             """.trimMargin(),
         )
         return
@@ -99,10 +100,18 @@ class CodeRunner : CliktCommand() {
 
 // DELETE THIS, THIS IS TEMPORARY
 val run = "-r"
-val testFile = "C:\\Users\\Usuario\\OneDrive\\Documentos\\Austral\\4to\\IngSis\\PrintScript\\testFile.txt"
 val format = "-f"
 val linter = "-l"
 val help = "-h"
-val output = "C:\\Users\\Usuario\\OneDrive\\Documentos\\Austral\\4to\\IngSis\\PrintScript\\outputFile.txt"
 
-fun main(args: Array<String>) = CodeRunner().main(arrayOf(run, testFile))
+// not pushed - will delete
+val testFile =
+    "C:\\Users\\Usuario\\OneDrive\\Documentos\\Austral\\4to\\IngSis\\PrintScript2\\src\\main\\resources\\testFile.txt"
+val output =
+    "C:\\Users\\Usuario\\OneDrive\\Documentos\\Austral\\4to\\IngSis\\PrintScript2\\src\\main\\resources\\outputFile.txt"
+val customFormatterRules =
+    "--rules=C:\\Users\\Usuario\\OneDrive\\Documentos\\Austral\\4to\\IngSis\\PrintScript2\\src\\main\\resources\\customFormatterRules.json"
+val customLinterRules =
+    "--rules=C:\\Users\\Usuario\\OneDrive\\Documentos\\Austral\\4to\\IngSis\\PrintScript2\\src\\main\\resources\\customLinterRules.json"
+
+fun main(args: Array<String>) = CodeRunner().main(arrayOf(format, customFormatterRules, testFile, output))
