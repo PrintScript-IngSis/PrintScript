ab95e07b9527c07093ec89b19cab5fd4c20b4598
AlanFierr0 <alan.fierro@ing.austral.edu.ar>
2024-05-08 16:35:28 -0300

    fix SwitchType

--- common/src/main/kotlin/SwitchType.kt
+++ common/src/main/kotlin/SwitchType.kt
@@ -5,19 +5,10 @@ import org.example.token.TokenType
 class SwitchType {
     companion object {
         fun typeToLiteral(type: TokenType): TokenType {
-            return when (type) {
-                TokenType.TYPE_NUMBER -> TokenType.LITERAL_NUMBER
-                TokenType.TYPE_STRING -> TokenType.LITERAL_STRING
-                TokenType.TYPE_BOOLEAN -> TokenType.LITERAL_BOOLEAN
-                else -> throw Exception("Unknown type")
-            }
-        }
-
-        fun literalToType(type: TokenType): TokenType {
-            return when (type) {
-                TokenType.LITERAL_NUMBER -> TokenType.TYPE_NUMBER
-                TokenType.LITERAL_STRING -> TokenType.TYPE_STRING
-                TokenType.LITERAL_BOOLEAN -> TokenType.TYPE_BOOLEAN
+            return when {
+                type in setOf(TokenType.TYPE_NUMBER, TokenType.LITERAL_NUMBER) -> TokenType.LITERAL_NUMBER
+                type in setOf(TokenType.TYPE_STRING, TokenType.LITERAL_STRING) -> TokenType.LITERAL_STRING
+                type in setOf(TokenType.TYPE_BOOLEAN, TokenType.LITERAL_BOOLEAN) -> TokenType.LITERAL_BOOLEAN
                 else -> throw Exception("Unknown type")
             }
         }
